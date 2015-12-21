b84b47a0980f4ed10bec5bae3a1b3a33afd2721c
Mike Spensieri <mikespensieri@smooch.io>
2015-12-21 10:10:06 -0500

    Version v3.2.3

--- Smooch.framework/Versions/A/Headers/Smooch.h
+++ Smooch.framework/Versions/A/Headers/Smooch.h
@@ -1,7 +1,7 @@
 //
 //  Smooch.h
 //  Smooch
-//  version : 3.2.2
+//  version : 3.2.3
 //
 //  Copyright (c) 2015 Smooch Technologies. All rights reserved.
 //
@@ -11,7 +11,7 @@
 #import "SKTSettings.h"
 #import "SKTUser.h"
 
-#define SMOOCH_VERSION @"3.2.2"
+#define SMOOCH_VERSION @"3.2.3"
 
 @interface Smooch : NSObject
 
