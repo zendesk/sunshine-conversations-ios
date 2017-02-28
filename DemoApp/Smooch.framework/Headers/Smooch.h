b27b78a2c95b303d48298613387294c402f89d5b
William Mora <william.r.mora@gmail.com>
2017-02-28 11:22:03 -0500

    Version v5.5.2

--- DemoApp/Smooch.framework/Headers/Smooch.h
+++ DemoApp/Smooch.framework/Headers/Smooch.h
@@ -1,7 +1,7 @@
 //
 //  Smooch.h
 //  Smooch
-//  version : 5.5.1
+//  version : 5.5.2
 //
 //  Copyright (c) 2015 Smooch Technologies. All rights reserved.
 //
@@ -14,7 +14,7 @@
 NS_ASSUME_NONNULL_BEGIN
 @protocol UNUserNotificationCenterDelegate;
 
-#define SMOOCH_VERSION @"5.5.1"
+#define SMOOCH_VERSION @"5.5.2"
 
 FOUNDATION_EXPORT double SmoochVersionNumber;
 FOUNDATION_EXPORT const unsigned char SmoochVersionString[];
