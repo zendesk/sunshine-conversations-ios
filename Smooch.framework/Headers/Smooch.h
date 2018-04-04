fcede68851d7403649d6d60f8f32e7f98a25900f
William Mora <william.r.mora@gmail.com>
2018-04-04 09:51:46 -0400

    Version v6.8.1

--- Smooch.framework/Headers/Smooch.h
+++ Smooch.framework/Headers/Smooch.h
@@ -2,7 +2,7 @@
 //  Smooch.h
 //  Smooch
 //
-//  version : 6.8.0
+//  version : 6.8.1
 
 #import <Foundation/Foundation.h>
 #import "SKTConversation.h"
@@ -12,7 +12,7 @@
 NS_ASSUME_NONNULL_BEGIN
 @protocol UNUserNotificationCenterDelegate;
 
-#define SMOOCH_VERSION @"6.8.0"
+#define SMOOCH_VERSION @"6.8.1"
 
 FOUNDATION_EXPORT double SmoochVersionNumber;
 FOUNDATION_EXPORT const unsigned char SmoochVersionString[];
