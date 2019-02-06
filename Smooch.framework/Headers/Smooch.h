55581dc53d2879311c3332ecefae49df8ba432f7
Mike Spensieri <mikespensieri@smooch.io>
2019-02-06 11:21:08 -0500

    Version v6.12.0

--- Smooch.framework/Headers/Smooch.h
+++ Smooch.framework/Headers/Smooch.h
@@ -2,7 +2,7 @@
 //  Smooch.h
 //  Smooch
 //
-//  version : 6.11.2
+//  version : 6.12.0
 
 #import <Foundation/Foundation.h>
 #import "SKTConversation.h"
@@ -12,7 +12,7 @@
 NS_ASSUME_NONNULL_BEGIN
 @protocol UNUserNotificationCenterDelegate;
 
-#define SMOOCH_VERSION @"6.11.2"
+#define SMOOCH_VERSION @"6.12.0"
 #define VENDOR_ID @"smooch"
 
 FOUNDATION_EXPORT double SmoochVersionNumber;
